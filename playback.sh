20390eccbe8e3b4e969f0d3d2ca3416f9ec3caa4
David Beitey <david@davidjb.com>
2018-12-04 12:35:18 +1000

    Make this work on Ubuntu 18.04

--- playback.sh
+++ playback.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
 mpv \
-    --rpi-background=yes \
-    --rpi-osd=no \
     --no-osc \
     --no-border \
     --no-window-dragging \
     --fullscreen \
     --fs-screen=all \
     --ontop \
-    --really-quiet \
     --loop-playlist \
-    --playlist /home/pi/.media_playlist > /dev/null 2>&1
+    --really-quiet \
+    --playlist /home/eresearch/.media_playlist > /dev/null 2>&1
