6a68582c4744c87ef61366250fa836c7d56a180c
David Beitey <david@davidjb.com>
2019-07-15 13:20:29 +1000

    Use short URL for default stream

--- install.sh
+++ install.sh
@@ -89,7 +89,7 @@ EOL
 DISPLAY=:0 pcmanfm --set-wallpaper="/home/pi/media-streaming-pi/img/coral-trout-and-buddy.jpg"
 
 # Create file for media playlist; default to Orpheus Island camera
-echo 'https://streamvdr-cns01.jcu.edu.au:1935/eResearch/UnderwaterCam.stream/playlist.m3u8' > /home/pi/.media_playlist
+echo 'https://jcu.io/video/orpheus-island-pioneer-bay-underwater' > /home/pi/.media_playlist
 
 # Set up systemd to run/restart the media stream
 sudo cp -f media-autoplay.service /etc/systemd/system/
