082f0f7080ee7be8070678c03b01a61d3a1ef35a
David Beitey <david@davidjb.com>
2019-07-16 05:35:36 +0000

    Use short URL for default camera

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ sudo apt install -y mpv python-pip
 sudo pip install youtube-dl
 
 # Create file for media playlist; default to Orpheus Island camera
-echo 'https://streamvdr-cns01.jcu.edu.au:1935/eResearch/UnderwaterCam.stream/playlist.m3u8' > $HOME/.media_playlist
+echo 'https://jcu.io/video/orpheus-island-pioneer-bay-underwater' > $HOME/.media_playlist
 
 # Set up systemd to run/restart the media stream
 sudo cp -f media-autoplay.service /etc/systemd/system/
